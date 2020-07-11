d9dea34c6199b9e7cee77c9780c56cbfb7f87600
vauxhall <zero.griffin@gmail.com>
2020-07-11 13:03:05 +0200

    Add segmented

--- macOS/Web.swift
+++ macOS/Web.swift
@@ -1,9 +1,10 @@
 import WebKit
+import Combine
 
 final class Web: NSView {
     override var mouseDownCanMoveWindow: Bool { true }
-    private weak var web: WKWebView!
     private weak var main: Main!
+    private var subs = Set<AnyCancellable>()
     
     required init?(coder: NSCoder) { nil }
     init(main: Main) {
@@ -16,6 +17,10 @@ final class Web: NSView {
         bar.material = .titlebar
         addSubview(bar)
         
+        let appareance = Segmented(items: [.key("Light"), .key("Dark")])
+        appareance.selected.value = effectiveAppearance == NSAppearance(named: .darkAqua) ? 1 : 0
+        bar.addSubview(appareance)
+        
         let separator = NSView()
         separator.translatesAutoresizingMaskIntoConstraints = false
         separator.wantsLayer = true
@@ -25,12 +30,15 @@ final class Web: NSView {
         let web = WKWebView(frame: .zero, configuration: .init())
         web.translatesAutoresizingMaskIntoConstraints = false
         addSubview(web)
-        self.web = web
         
         bar.topAnchor.constraint(equalTo: topAnchor).isActive = true
         bar.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         bar.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         bar.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        appareance.centerYAnchor.constraint(equalTo: bar.centerYAnchor, constant: -1).isActive = true
+        appareance.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
+        appareance.widthAnchor.constraint(equalToConstant: 200).isActive = true
         
         separator.topAnchor.constraint(equalTo: bar.bottomAnchor).isActive = true
         separator.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
@@ -42,10 +50,10 @@ final class Web: NSView {
         web.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         web.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         
-        refresh()
-    }
-    
-    private func refresh() {
         web.load(.init(url: main.url.appendingPathComponent("index.html")))
+        
+        appareance.selected.sink {
+            web.appearance = $0 == 0 ? NSAppearance(named: .aqua) : NSAppearance(named: .darkAqua)
+        }.store(in: &subs)
     }
 }
