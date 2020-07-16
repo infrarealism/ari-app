c7de549f1883b3309abfa075a6f06141e9f618e9
vauxhall <zero.griffin@gmail.com>
2020-07-16 14:06:22 +0200

    Update pages

--- macOS/Create.swift
+++ macOS/Create.swift
@@ -36,62 +36,42 @@ final class Create: NSWindow {
         effect.material = .hudWindow
         contentView = effect
         
-        let title = Label(.key("New.website"), .bold(6))
-        effect.addSubview(title)
-        
-        
-        
-        
-        
-        let first = NSView()
-        first.translatesAutoresizingMaskIntoConstraints = false
-        effect.addSubview(first)
-        
-        let second = NSView()
-        second.translatesAutoresizingMaskIntoConstraints = false
-        effect.addSubview(second)
-        
-        let third = NSView()
-        third.translatesAutoresizingMaskIntoConstraints = false
-        effect.addSubview(third)
-        
-        let fourth = NSView()
-        fourth.translatesAutoresizingMaskIntoConstraints = false
-        effect.addSubview(fourth)
+        let items = Array(repeating: NSView(), count: 4)
+        let pages = Pages(title: .key("New.website"), pages: items)
         
         let enterName = Label(.key("Enter.name"), .medium())
-        first.addSubview(enterName)
+        items[0].addSubview(enterName)
         
         let name = Field()
         name.placeholderString = .key("Website.name")
-        first.addSubview(name)
+        items[0].addSubview(name)
         self.name = name
         
         let firstNext = Button(icon: "arrow.right.circle.fill", color: .systemBlue)
         firstNext.target = self
         firstNext.action = #selector(next)
-        first.addSubview(firstNext)
+        items[0].addSubview(firstNext)
         
         let enterType = Label(.key("Enter.type"), .medium())
-        second.addSubview(enterType)
+        items[1].addSubview(enterType)
         
         let singleSegment = Segment(icon: "dot.square.fill", title: .key("Single"))
         singleSegment.selected = true
         singleSegment.target = self
         singleSegment.action = #selector(single)
-        second.addSubview(singleSegment)
+        items[1].addSubview(singleSegment)
         self.singleSegment = singleSegment
         
         let blogSegment = Segment(icon: "square.stack.3d.up.fill", title: .key("Blog"))
         blogSegment.target = self
         blogSegment.action = #selector(blog)
-        second.addSubview(blogSegment)
+        items[1].addSubview(blogSegment)
         self.blogSegment = blogSegment
         
         let secondNext = Button(icon: "arrow.right.circle.fill", color: .systemBlue)
         secondNext.target = self
         secondNext.action = #selector(next)
-        second.addSubview(secondNext)
+        items[1].addSubview(secondNext)
         
         let secondPrevious = Button(icon: "arrow.left.circle.fill", color: .systemBlue)
         secondPrevious.target = self
@@ -137,40 +117,7 @@ final class Create: NSWindow {
         finish.action = #selector(self.finish)
         fourth.addSubview(finish)
         
-        first.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
-        first.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
-        first.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
-        offset = first.leftAnchor.constraint(equalTo: effect.leftAnchor)
-        offset.isActive = true
         
-        second.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
-        second.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
-        second.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
-        second.leftAnchor.constraint(equalTo: first.rightAnchor).isActive = true
-        
-        third.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
-        third.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
-        third.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
-        third.leftAnchor.constraint(equalTo: second.rightAnchor).isActive = true
-        
-        fourth.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
-        fourth.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
-        fourth.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
-        fourth.leftAnchor.constraint(equalTo: third.rightAnchor).isActive = true
-        
-        title.topAnchor.constraint(equalTo: effect.topAnchor, constant: 50).isActive = true
-        title.leftAnchor.constraint(equalTo: effect.leftAnchor, constant: 20).isActive = true
-        
-        progress.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 6).isActive = true
-        progress.leftAnchor.constraint(equalTo: title.leftAnchor, constant: 3).isActive = true
-        progress.widthAnchor.constraint(equalToConstant: 354).isActive = true
-        progress.heightAnchor.constraint(equalToConstant: 6).isActive = true
-        
-        bar.topAnchor.constraint(equalTo: progress.topAnchor).isActive = true
-        bar.leftAnchor.constraint(equalTo: progress.leftAnchor).isActive = true
-        bar.bottomAnchor.constraint(equalTo: progress.bottomAnchor).isActive = true
-        self.progress = bar.widthAnchor.constraint(equalToConstant: 0)
-        self.progress.isActive = true
         
         enterName.topAnchor.constraint(equalTo: first.topAnchor, constant: 100).isActive = true
         enterName.leftAnchor.constraint(equalTo: first.leftAnchor, constant: 20).isActive = true
--- macOS/Pages.swift
+++ macOS/Pages.swift
@@ -21,5 +21,40 @@ final class Pages: NSView {
         bar.wantsLayer = true
         bar.layer!.backgroundColor = NSColor.systemBlue.cgColor
         progress.addSubview(bar)
+        
+        first.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
+        first.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
+        first.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
+        offset = first.leftAnchor.constraint(equalTo: effect.leftAnchor)
+        offset.isActive = true
+        
+        second.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
+        second.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
+        second.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
+        second.leftAnchor.constraint(equalTo: first.rightAnchor).isActive = true
+        
+        third.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
+        third.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
+        third.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
+        third.leftAnchor.constraint(equalTo: second.rightAnchor).isActive = true
+        
+        fourth.topAnchor.constraint(equalTo: effect.topAnchor).isActive = true
+        fourth.bottomAnchor.constraint(equalTo: effect.bottomAnchor).isActive = true
+        fourth.widthAnchor.constraint(equalTo: effect.widthAnchor).isActive = true
+        fourth.leftAnchor.constraint(equalTo: third.rightAnchor).isActive = true
+        
+        title.topAnchor.constraint(equalTo: effect.topAnchor, constant: 50).isActive = true
+        title.leftAnchor.constraint(equalTo: effect.leftAnchor, constant: 20).isActive = true
+        
+        progress.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 6).isActive = true
+        progress.leftAnchor.constraint(equalTo: title.leftAnchor, constant: 3).isActive = true
+        progress.widthAnchor.constraint(equalToConstant: 354).isActive = true
+        progress.heightAnchor.constraint(equalToConstant: 6).isActive = true
+        
+        bar.topAnchor.constraint(equalTo: progress.topAnchor).isActive = true
+        bar.leftAnchor.constraint(equalTo: progress.leftAnchor).isActive = true
+        bar.bottomAnchor.constraint(equalTo: progress.bottomAnchor).isActive = true
+        self.progress = bar.widthAnchor.constraint(equalToConstant: 0)
+        self.progress.isActive = true
     }
 }
