a4b2ab4741026460da3582ca12de418f1e7321f4
vauxhall <zero.griffin@gmail.com>
2020-08-03 17:24:34 +0200

    Create launch

--- iOS/App.swift
+++ iOS/App.swift
@@ -3,10 +3,6 @@ import UIKit
 @UIApplicationMain final class App: NSObject, UIApplicationDelegate {
     var windows = Set<UIWindow>()
     
-    func applicationDidFinishLaunching(_: UIApplication) {
-        UISegmentedControl.appearance().selectedSegmentTintColor = .systemPink
-    }
-    
     func application(_: UIApplication, didDiscardSceneSessions: Set<UISceneSession>) {
         windows = windows.filter {
             didDiscardSceneSessions.contains($0.windowScene!.session)
---
+++ iOS/Launch.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct Launch: View {
+    var body: some View {
+        TabView {
+            Circle()
+                .tabItem {
+                    Image(systemName: "app.fill")
+                    Text("Home")
+                }
+            Circle()
+                .tabItem {
+                    Image(systemName: "textformat.size")
+                    Text("Type")
+                }
+            Circle()
+                .tabItem {
+                    Image(systemName: "circle.lefthalf.fill")
+                    Text("Colors")
+                }
+            Circle()
+                .tabItem {
+                    Image(systemName: "slider.horizontal.3")
+                    Text("Features")
+                }
+            Circle()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+        }.accentColor(.pink)
+    }
+}
