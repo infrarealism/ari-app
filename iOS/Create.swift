65a0a5a1a099b2946032fee9b4cfff20ecb915a3
vauxhall <zero.griffin@gmail.com>
2020-08-05 13:52:41 +0200

    Update create

--- iOS/Create.swift
+++ iOS/Create.swift
@@ -8,16 +8,30 @@ struct Create: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                HStack(spacing: 0) {
-                    First(offset: self.$offset, name: self.$name, window: self.window)
-                        .frame(width: geo.size.width, height: geo.size.height)
-                    Circle()
-                        .frame(width: geo.size.width, height: geo.size.height)
-                    Rectangle()
-                        .frame(width: geo.size.width, height: geo.size.height)
-                }.frame(width: geo.size.width, height: geo.size.height, alignment: .leading)
-                    .offset(x: geo.size.width * -self.offset)
                 VStack(spacing: 0) {
+                    HStack {
+                        Spacer()
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 2)
+                                .frame(height: 4)
+                                .foregroundColor(.init(.secondarySystemBackground))
+                                .frame(width: 200)
+                            HStack {
+                                RoundedRectangle(cornerRadius: 3)
+                                    .frame(height: 6)
+                                    .foregroundColor(.pink)
+                                    .frame(width: CGFloat(200) / 3 * self.offset)
+                                        Spacer()
+                                Spacer()
+                            }
+                        }.frame(width: 200)
+                            .padding(.top, 10)
+                        Spacer()
+                    }
+                    Text("New.website")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                        .padding()
                     Spacer()
                     HStack {
                         Button(action: {
@@ -57,6 +71,15 @@ struct Create: View {
                         .frame(height: 50)
                         .padding(.bottom, 20)
                 }
+                HStack(spacing: 0) {
+                    First(offset: self.$offset, name: self.$name, window: self.window)
+                        .frame(width: geo.size.width, height: geo.size.height)
+                    Circle()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                    Rectangle()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                }.frame(width: geo.size.width, height: geo.size.height, alignment: .leading)
+                    .offset(x: geo.size.width * -self.offset)
             }
         }
     }
@@ -68,21 +91,19 @@ private struct First: View {
     weak var window: UIWindow!
     
     var body: some View {
-        VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 220, height: 40)
-                    .foregroundColor(.init(.secondarySystemBackground))
-                HStack {
-                    Spacer()
-                    TextField("Website.name", text: $name) {
-                        self.window.endEditing(true)
-                    }.autocapitalization(.sentences)
-                        .multilineTextAlignment(.center)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .frame(width: 200, height: 40)
-                    Spacer()
-                }
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 220, height: 40)
+                .foregroundColor(.init(.secondarySystemBackground))
+            HStack {
+                Spacer()
+                TextField("Website.name", text: $name) {
+                    self.window.endEditing(true)
+                }.autocapitalization(.sentences)
+                    .multilineTextAlignment(.center)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .frame(width: 200, height: 40)
+                Spacer()
             }
         }
     }
