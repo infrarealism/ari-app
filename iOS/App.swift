2d65abaae03cfdc67aaa9db5fa4999c836061088
vauxhall <zero.griffin@gmail.com>
2020-08-06 17:30:34 +0200

    Load session on launch

--- iOS/App.swift
+++ iOS/App.swift
@@ -5,6 +5,10 @@ let session = Session()
 @UIApplicationMain final class App: NSObject, UIApplicationDelegate {
     var windows = Set<UIWindow>()
     
+    func applicationDidFinishLaunching(_: UIApplication) {
+        session.load()
+    }
+    
     func application(_: UIApplication, didDiscardSceneSessions: Set<UISceneSession>) {
         windows = windows.filter {
             !didDiscardSceneSessions.contains($0.windowScene!.session)
