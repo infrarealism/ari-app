dee900e0ec16e9faa3cbf7ad282e564fbfec8046
vauxhall <zero.griffin@gmail.com>
2020-08-11 19:25:56 +0200

    Update design

--- iOS/Design.swift
+++ iOS/Design.swift
@@ -3,64 +3,76 @@ import Core
 
 struct Design: View {
     weak var website: Website!
+    @State private var primary: Core.Color!
+    @State private var secondary: Core.Color!
     
     var body: some View {
         NavigationView {
             List {
-                Section(footer: Picker(selected: website.model.style.primary) {
-                    self.website.model.style.primary = $0
-                }) {
-                    VStack {
-                            HStack {
-                                Text("Primary.title")
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                                Spacer()
-                            }
-                            HStack {
-                                Text("Primary.description")
-                                    .font(.callout)
-                                    .foregroundColor(.secondary)
-                                Spacer()
-                            }
+                if primary != nil {
+                    Section(footer: Picker(selected: $primary)) {
+                        VStack {
+                                HStack {
+                                    Text("Primary.title")
+                                        .font(.headline)
+                                        .foregroundColor(.primary)
+                                    Spacer()
+                                }
+                                HStack {
+                                    Text("Primary.description")
+                                        .font(.callout)
+                                        .foregroundColor(.secondary)
+                                    Spacer()
+                                }
+                        }
                     }
                 }
-                Section(footer: Picker(selected: website.model.style.secondary){
-                    self.website.model.style.secondary = $0
-                }) {
-                    VStack {
-                            HStack {
-                                Text("Secondary.title")
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                                Spacer()
-                            }
-                            HStack {
-                                Text("Secondary.description")
-                                    .font(.callout)
-                                    .foregroundColor(.secondary)
-                                Spacer()
-                            }
+                if secondary != nil {
+                    Section(footer: Picker(selected: $secondary)) {
+                        VStack {
+                                HStack {
+                                    Text("Secondary.title")
+                                        .font(.headline)
+                                        .foregroundColor(.primary)
+                                    Spacer()
+                                }
+                                HStack {
+                                    Text("Secondary.description")
+                                        .font(.callout)
+                                        .foregroundColor(.secondary)
+                                    Spacer()
+                                }
+                        }
                     }
                 }
             }.listStyle(GroupedListStyle())
                 .navigationBarTitle("Colours", displayMode: .large)
         }.navigationViewStyle(StackNavigationViewStyle())
+            .onAppear {
+                self.primary = self.website.model.style.primary
+                self.secondary = self.website.model.style.secondary
+        }.onReceive([primary].publisher) {
+            guard let color = $0, self.website.model.style.primary != color else { return }
+            self.website.model.style.primary = color
+            
+        }.onReceive([secondary].publisher) {
+            guard let color = $0, self.website.model.style.secondary != color else { return }
+            self.website.model.style.secondary = color
+        }
     }
 }
 
 private struct Picker: View {
-    @State var selected: Core.Color
-    let action: (Core.Color) -> Void
+    @Binding var selected: Core.Color!
     
     var body: some View {
         HStack {
             Spacer()
-            Item(color: .blue, selected: $selected, action: action)
-            Item(color: .indigo, selected: $selected, action: action)
-            Item(color: .purple, selected: $selected, action: action)
-            Item(color: .pink, selected: $selected, action: action)
-            Item(color: .orange, selected: $selected, action: action)
+            Item(color: .blue, selected: $selected)
+            Item(color: .indigo, selected: $selected)
+            Item(color: .purple, selected: $selected)
+            Item(color: .pink, selected: $selected)
+            Item(color: .orange, selected: $selected)
             Spacer()
         }.padding()
     }
@@ -68,15 +80,13 @@ private struct Picker: View {
 
 private struct Item: View {
     let color: Core.Color
-    @Binding var selected: Core.Color
-    let action: (Core.Color) -> Void
+    @Binding var selected: Core.Color!
     
     var body: some View {
         Button(action: {
             withAnimation {
                 self.selected = self.color
             }
-            self.action(self.color)
         }) {
             RoundedRectangle(cornerRadius: 6)
                 .foregroundColor(.init(color.color))
