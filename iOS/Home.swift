661873fcd3aaa36165522edfb68fae80efcf5714
vauxhall <zero.griffin@gmail.com>
2020-08-15 10:23:50 +0200

    Action sheet

--- iOS/Home.swift
+++ iOS/Home.swift
@@ -4,11 +4,34 @@ import Core
 struct Home: View {
     weak var window: UIWindow!
     weak var website: Website!
+    @State private var project = false
+    @State private var folder = false
     
     var body: some View {
         NavigationView {
             List {
-                EmptyView()
+                Section(header:
+                    HStack {
+                        Spacer()
+                        Image(systemName: "square.and.arrow.up.fill")
+                            .font(.title)
+                            .foregroundColor(.pink)
+                            .padding()
+                        Spacer()
+                }) {
+                    Button(action: {
+                        self.project = true
+                    }) {
+                        Text("Share.project")
+                    }.actionSheet(isPresented: $project) {
+                        ActionSheet(title: <#T##Text#>, message: <#T##Text?#>, buttons: <#T##[ActionSheet.Button]#>)
+                    }
+                    Button(action: {
+                        self.folder = true
+                    }) {
+                        Text("Share.folder")
+                    }
+                }
             }.listStyle(GroupedListStyle())
                 .navigationBarTitle(Text(verbatim: website.model.name), displayMode: .large)
                 .navigationBarItems(trailing: Button(action: {
